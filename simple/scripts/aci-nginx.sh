69d3085814768cdaf6f668ca8602463bdb31b02c
Toru Makabe <tomakabe@microsoft.com>
2019-02-04 16:28:09 +0900

    add location

--- simple/scripts/aci-nginx.sh
+++ simple/scripts/aci-nginx.sh
@@ -4,6 +4,7 @@ YOUR_PREFIX="YOUR PREFIX"
 
 az container create \
     -g handson-container-aci-rg \
+    -l westus2 \
     -n ${YOUR_PREFIX}-nginx \
     --image nginx \
     --ip-address public
--- simple/scripts/aci-wordcount.sh
+++ simple/scripts/aci-wordcount.sh
@@ -4,15 +4,18 @@ YOUR_PREFIX="YOUR PREFIX"
 
 az container create \
     -g handson-container-aci-rg \
+    -l westus2 \
     -n ${YOUR_PREFIX}-wordcount \
     --image microsoft/aci-wordcount:latest \
     --restart-policy OnFailure
 
 az container show \
     -g handson-container-aci-rg \
+    -l westus2 \
     -n ${YOUR_PREFIX}-wordcount \
     --query containers[0].instanceView.currentState.state
 
 az container logs \
     -g handson-container-aci-rg \
+    -l westus2 \
     -n ${YOUR_PREFIX}-wordcount
