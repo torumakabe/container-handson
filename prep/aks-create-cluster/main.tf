e3f04f79e2325f3c14195b1e9bc612d9d82dd776
ToruMakabe <tomakabe@MBP2018.local>
2019-05-27 21:16:22 +0900

    change variables order

--- prep/aks-create-cluster/main.tf
+++ prep/aks-create-cluster/main.tf
@@ -6,12 +6,12 @@ module "network" {
 }
 
 module "aks" {
-  source        = "../modules/aks"
-  aks_subnet_id = module.network.aks_subnet_id
+  source = "../modules/aks"
 
   aks_cluster_name                = var.aks_cluster_name
   aks_cluster_rg                  = var.aks_cluster_rg
   aks_cluster_location            = var.aks_cluster_location
+  aks_subnet_id                   = module.network.aks_subnet_id
   service_principal               = var.service_principal
   service_principal_client_secret = var.service_principal_client_secret
   la_workspace_name_for_aks       = var.la_workspace_name_for_aks
