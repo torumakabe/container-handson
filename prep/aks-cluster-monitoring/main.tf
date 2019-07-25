3588dbc97883fe6b97926394c872298aa3aa2da1
Toru Makabe <tomakabe@microsoft.com>
2019-07-25 09:32:23 +0900

    upgrade Azure RM provider -> 1.32

--- prep/aks-cluster-monitoring/main.tf
+++ prep/aks-cluster-monitoring/main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 
 provider "azurerm" {
-  version = "~>1.31"
+  version = "~>1.32"
 }
 
 provider "azuread" {
--- prep/modules/rg/main.tf
+++ prep/modules/rg/main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = "~>1.31"
+  version = "~>1.32"
 }
 
 resource "azurerm_resource_group" "aks" {
