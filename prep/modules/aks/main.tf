39b985b98e88c4899f5cbbe72a31bbbb20db5a55
ToruMakabe <torumakabe@microsoft.com>
2020-02-29 08:43:53 +0900

    upgrade k8s provider -> 1.11

--- prep/modules/aks/main.tf
+++ prep/modules/aks/main.tf
@@ -104,7 +104,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 }
 
 provider "kubernetes" {
-  version = "~>1.10.0"
+  version = "~>1.11"
 
   load_config_file       = false
   host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
