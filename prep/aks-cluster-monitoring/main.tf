ef9f4a14f5e59f854ba3ad0ae84dfce412a04954
ToruMakabe <tomakabe@microsoft.com>
2020-04-03 10:52:49 +0900

    upgrade AzureRM provider -> 2.4

--- prep/aks-cluster-monitoring/main.tf
+++ prep/aks-cluster-monitoring/main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 
 provider "azurerm" {
-  version = "~>2.3"
+  version = "~>2.4"
   features {}
 }
 
--- prep/modules/aks/main.tf
+++ prep/modules/aks/main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = "~>2.3"
+  version = "~>2.4"
   features {}
 }
 
